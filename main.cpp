ec4ffc06720ba3d66fab5507e4acd2afa6c57727
Thomas Tran <timtom9801@gmail.com>
2022-05-17 13:19:25 -0700

    Part 1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include "main.h"
 
-double leaveOneOutCrossValidation(int data, vector<int>current_set_of_features, vector<int>feature_to_add){
+double leaveOneOutCrossValidation(int data, vector<int>current_set_of_features, int feature_to_add){
     int accuracy = rand() % 101; //range of 0 - 100 (whole number)
     int dec = rand() % 9; //0 - 9 (decimal part)
     string whole = to_string(accuracy);
@@ -10,20 +10,26 @@ double leaveOneOutCrossValidation(int data, vector<int>current_set_of_features, 
     return val; 
 }
 
+void printCurrSet(vector<int> current_set_of_features){
+    for (int i = 0; i < current_set_of_features.size(); i++){
+        cout << current_set_of_features.at(i) << ",";
+    }
+}
+
 void feature_search_demo(int data){ //for NOW will pass in the number of rows
     int num = data; //data is the number of features for NOW
     vector<int> current_set_of_features; //initialize empty set
-    vector<int> feature_to_add_at_this_level;
+    int feature_to_add_at_this_level;
     double best_so_far_accuracy = 0;
     double accuracy = 0;
     bool featureNotInSet = true;
 
     for (int i = 1; i <= num; i++ ){
-        cout << "On the " << i << "th level of the search tree" << endl;
-        feature_to_add_at_this_level; //empty vector
+        // cout << "On the " << i << "th level of the search tree" << endl;
+        feature_to_add_at_this_level = 0;//empty vector
         best_so_far_accuracy = 0;
-        featureNotInSet = true;
         for (int j = 1; j <= num; j++ ){
+                featureNotInSet = true;
                 for (int feature : current_set_of_features){ //loop through current_set_of_features and see if the current feature(j) is already in it, if not  bool = true
                     if (j == feature){ //means that curr feature j already in
                         featureNotInSet = false;
@@ -31,8 +37,12 @@ void feature_search_demo(int data){ //for NOW will pass in the number of rows
                     } 
                 } 
                 if (featureNotInSet){
-                    cout << "--Considering adding the " << j << " feature" << endl;
+                    // cout << "--Considering adding the " << j << " feature" << endl;
+                    //"Using feature(s) {1} accuracy is 35.4%"
                     accuracy = leaveOneOutCrossValidation(data, current_set_of_features, j);
+                    cout << "Using feature(s) {";
+                    printCurrSet(current_set_of_features);
+                    cout << j << "} accuracy is " << accuracy << "%" << endl;
                     
 
                     if (accuracy > best_so_far_accuracy){
@@ -42,8 +52,12 @@ void feature_search_demo(int data){ //for NOW will pass in the number of rows
                 }
         }
         
+        cout << "\nFeature set {";
+        printCurrSet(current_set_of_features);
+        cout << feature_to_add_at_this_level << "} was best, accuracy was " << best_so_far_accuracy << "%\n" <<endl; 
         current_set_of_features.push_back(feature_to_add_at_this_level);
-        cout <<"On level " << i << " I added feature " << feature_to_add_at_this_level << " to current set\n" << endl;
+        // cout <<"On level " << i << " I added feature " << feature_to_add_at_this_level << " to current set\n" << endl;
+        //Feature set {1,2} was best, accuracy was 58.9%
     }
 }
 
--- main.h
+++ main.h
@@ -9,5 +9,6 @@
 using namespace std;
 
 // double leaveOneOutCrossValidation(int feature_to_add);
-double leaveOneOutCrossValidation(int data, vector<int>current_set_of_features, vector<int>feature_to_add);
+double leaveOneOutCrossValidation(int data, vector<int>current_set_of_features, int feature_to_add);
 void feature_search_demo(int data); //for now data represents number of rows
+void printCurrSet(vector<int> current_set_of_features);
