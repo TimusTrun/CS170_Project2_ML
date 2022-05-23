a7f28c9789adb3dd4b3eeb6c23e94cdededcc927
Thomas Tran <timtom9801@gmail.com>
2022-05-23 02:42:35 -0700

    Progress on part 2

--- main.cpp
+++ main.cpp
@@ -1,13 +1,100 @@
 #include "main.h"
 
-double leaveOneOutCrossValidation(int data, vector<int>current_set_of_features, int feature_to_add){
-    int accuracy = rand() % 101; //range of 0 - 100 (whole number)
-    int dec = rand() % 9; //0 - 9 (decimal part)
-    string whole = to_string(accuracy);
-    string decimal = to_string(dec);
-    string num = whole + "." + decimal;
-    double val = stod(num);
-    return val; 
+
+
+void printRow(vector<double> row){
+    for (int j = 0; j < row.size(); j++){
+        cout << row.at(j) << endl;
+    }
+}
+
+vector<vector<double>> convertFiletoData(string data){
+    string fileName = data; //user picks either large dataset or small "small-test-dataset.txt"
+    cout << fileName << endl;
+    ifstream fin;  //initialize datastream
+    string line;
+    vector<string> temp = {}; //use to store the rows in (all numbers separated)
+    int i = 0;
+    vector<vector<string>> fileInput; //vector that holds each row and each number is an element of the row in string form
+    vector<vector<double>> fileInputDoubles; //vector that holds each row in form of doubles
+
+    fin.open(fileName); //opens file
+
+    while (fin) { //loops until reaches end of file
+        getline(fin, line); //reads file line by line "2.0000000e+000  5.0688373e+000  3.1403378e+000  1.2891084e+000  5.7634216e-001  2.7268616e+000  1.9103920e+000  4.6027145e+000  3.3621920e+000  4.0253170e+000  2.8503988e+000"
+        string s(line);//converted char* to string
+        istringstream iss(s);
+        do { //taking row and breaking it up into individual elements
+            string word;
+            iss >> word;
+            
+            if (word == "\n"){
+                //if newline detected, do nothing
+            } else {
+                // cout << word << endl; 
+                temp.push_back(word);
+            }
+        } while(iss); 
+        fileInput.push_back(temp); //push back the row
+        // printRow(fileInput.at(i));
+        i++;
+        temp = {};
+    }
+    
+    fin.close(); // now we have all rows into a 2d vector in fileInput as strings
+    for (int i = 0; i < fileInput.size(); i++) { //resize each row to be 11
+        fileInput.at(i).resize(11);
+        // cout << "Size at row " << i << " is " << fileInput.at(i).size() << endl;
+    } 
+
+    vector<double> temp2 = {};
+    for (int i = 0; i < fileInput.size()-1; i++){ //turn all strings into doubles and store into double array
+        for (int j = 0; j < fileInput.at(0).size(); j++){
+            string s = fileInput.at(i).at(j);
+            istringstream os(s);
+            double d;
+            os >> d;
+            temp2.push_back(d);
+            // cout << d << endl;
+        }
+        fileInputDoubles.push_back(temp2); //pushing rows into vector
+        temp2 = {};
+    }
+    return fileInputDoubles;
+}
+
+double leaveOneOutCrossValidation(vector<vector<double>> data, vector<int>current_set_of_features, int feature_to_add){
+    // int accuracy = rand() % 101; //range of 0 - 100 (whole number)
+    // int dec = rand() % 9; //0 - 9 (decimal part)
+    // string whole = to_string(accuracy);
+    // string decimal = to_string(dec);
+    // string num = whole + "." + decimal;
+    // double val = stod(num);
+    // return val;
+    vector<double> objectToClassify;
+    double labelObjectToClassify;
+    double nearestNeighborDistance = INT_MAX;
+    double nearestNeighborLocation = INT_MAX;
+    
+    for (int i = 0; i < data.size(); i++){
+        for (int x = 1; x < data.at(0).size(); x++){ //all features of the ith object
+            objectToClassify.push_back(data.at(i).at(x));
+        }
+        labelObjectToClassify = data.at(i).at(0); //first index of each row, identifier
+        
+        nearestNeighborDistance = INT_MAX;
+        nearestNeighborLocation = INT_MAX;
+        
+        for (int j = 0; j < data.size(); j++){
+            if (j != i){ // cout << "Ask if " << i << " is nearest neighbor with " << j << endl;
+                for (int x = 0; x < data.at(0).size(); x++){ //calculating distance for NN
+                    //TODO: FINISH EUCLIDEAN DISTANCES **************************************************************
+                } //calculating distance for NN
+            }
+        }
+
+    }
+    return 0;
 }
 
 void printCurrSet(vector<int> current_set_of_features){
@@ -16,67 +103,78 @@ void printCurrSet(vector<int> current_set_of_features){
     }
 }
 
-void feature_search_demo(int data){ //for NOW will pass in the number of rows
-    int num = data; //data is the number of features for NOW
-    vector<int> current_set_of_features; //initialize empty set
-    int feature_to_add_at_this_level;
-    double best_so_far_accuracy = 0;
-    double accuracy = 0;
-    bool featureNotInSet = true;
+// void feature_search_demo(string data){ //string for file name
+//     // int num = data; //data is the number of features for NOW
+//     vector<int> current_set_of_features; //initialize empty set
+//     int feature_to_add_at_this_level;
+//     double best_so_far_accuracy = 0;
+//     double accuracy = 0;
+//     bool featureNotInSet = true;
 
-    for (int i = 1; i <= num; i++ ){
-        // cout << "On the " << i << "th level of the search tree" << endl;
-        feature_to_add_at_this_level = 0;//empty vector
-        best_so_far_accuracy = 0;
-        for (int j = 1; j <= num; j++ ){
-                featureNotInSet = true;
-                for (int feature : current_set_of_features){ //loop through current_set_of_features and see if the current feature(j) is already in it, if not  bool = true
-                    if (j == feature){ //means that curr feature j already in
-                        featureNotInSet = false;
-                        break;
-                    } 
-                } 
-                if (featureNotInSet){
-                    // cout << "--Considering adding the " << j << " feature" << endl;
-                    //"Using feature(s) {1} accuracy is 35.4%"
-                    accuracy = leaveOneOutCrossValidation(data, current_set_of_features, j);
-                    cout << "Using feature(s) {";
-                    printCurrSet(current_set_of_features);
-                    cout << j << "} accuracy is " << accuracy << "%" << endl;
+//     for (int i = 1; i <= num; i++ ){
+//         // cout << "On the " << i << "th level of the search tree" << endl;
+//         feature_to_add_at_this_level = 0;//empty vector
+//         best_so_far_accuracy = 0;
+//         for (int j = 1; j <= num; j++ ){
+//                 featureNotInSet = true;
+//                 for (int feature : current_set_of_features){ //loop through current_set_of_features and see if the current feature(j) is already in it, if not  bool = true
+//                     if (j == feature){ //means that curr feature j already in
+//                         featureNotInSet = false;
+//                         break;
+//                     } 
+//                 } 
+//                 if (featureNotInSet){
+//                     // cout << "--Considering adding the " << j << " feature" << endl;
+//                     //"Using feature(s) {1} accuracy is 35.4%"
+//                     accuracy = leaveOneOutCrossValidation(data, current_set_of_features, j);
+//                     cout << "Using feature(s) {";
+//                     printCurrSet(current_set_of_features);
+//                     cout << j << "} accuracy is " << accuracy << "%" << endl;
                     
 
-                    if (accuracy > best_so_far_accuracy){
-                        best_so_far_accuracy = accuracy;
-                        feature_to_add_at_this_level = j;
-                    }
-                }
-        }
+//                     if (accuracy > best_so_far_accuracy){
+//                         best_so_far_accuracy = accuracy;
+//                         feature_to_add_at_this_level = j;
+//                     }
+//                 }
+//         }
         
-        cout << "\nFeature set {";
-        printCurrSet(current_set_of_features);
-        cout << feature_to_add_at_this_level << "} was best, accuracy was " << best_so_far_accuracy << "%\n" <<endl; 
-        current_set_of_features.push_back(feature_to_add_at_this_level);
-        // cout <<"On level " << i << " I added feature " << feature_to_add_at_this_level << " to current set\n" << endl;
-        //Feature set {1,2} was best, accuracy was 58.9%
-    }
-}
+//         cout << "\nFeature set {";
+//         printCurrSet(current_set_of_features);
+//         cout << feature_to_add_at_this_level << "} was best, accuracy was " << best_so_far_accuracy << "%\n" <<endl; 
+//         current_set_of_features.push_back(feature_to_add_at_this_level);
+//         // cout <<"On level " << i << " I added feature " << feature_to_add_at_this_level << " to current set\n" << endl;
+//         //Feature set {1,2} was best, accuracy was 58.9%
+//     }
+// }
 
-int main(){
+// int main(){ //part 1
+//     vector<int> placeholder;
+
+//     cout << "Welcome to Thomas Tran' Feature Selection Algorithm\n" << endl;
+//     cout << "Please enter the total number of features" << endl;
+//     int numOfFeatures;
+//     cin >> numOfFeatures;
+
+//     cout << "Type the number of the algorithm you want to run.\n\n - Forward Selection(1) \n - Backward Selection(2) \n - Thomas' Special Algorithm(3)\n" << endl;
+//     int algo;
+//     cin >> algo;
+
+//     cout << "Using no features and \"random\" evaluation, I get an accuracy of " << leaveOneOutCrossValidation(numOfFeatures, placeholder, 0) << "%\n" << endl;
+//     cout << "Beginning search.\n" << endl;
+
+//     feature_search_demo(numOfFeatures);
+
+//     return 0;
+// }
+
+int main(){ //part 2
+    string fileName = "small-test-dataset.txt";
+    cout << "HIIII" <<endl;
     vector<int> placeholder;
-
-    cout << "Welcome to Thomas Tran' Feature Selection Algorithm\n" << endl;
-    cout << "Please enter the total number of features" << endl;
-    int numOfFeatures;
-    cin >> numOfFeatures;
-
-    cout << "Type the number of the algorithm you want to run.\n\n - Forward Selection(1) \n - Backward Selection(2) \n - Thomas' Special Algorithm(3)\n" << endl;
-    int algo;
-    cin >> algo;
-
-    cout << "Using no features and \"random\" evaluation, I get an accuracy of " << leaveOneOutCrossValidation(numOfFeatures, placeholder, 0) << "%\n" << endl;
-    cout << "Beginning search.\n" << endl;
-
-    feature_search_demo(numOfFeatures);
-
+    vector<vector<double>> data;
+    data = convertFiletoData(fileName);
+    leaveOneOutCrossValidation(data, placeholder, 0);
+    
     return 0;
 }
--- main.h
+++ main.h
@@ -6,9 +6,14 @@
 #include <vector>
 #include <algorithm>
 #include <string>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <limits.h>
 using namespace std;
 
-// double leaveOneOutCrossValidation(int feature_to_add);
-double leaveOneOutCrossValidation(int data, vector<int>current_set_of_features, int feature_to_add);
-void feature_search_demo(int data); //for now data represents number of rows
+double leaveOneOutCrossValidation(vector<vector<double>> data, vector<int>current_set_of_features, int feature_to_add); //k-fold cross validation
+void feature_search_demo(string data); //data is the input file 
 void printCurrSet(vector<int> current_set_of_features);
+void printRow(vector<double> row);
+vector<vector<double>> convertFiletoData(string data); // input file and turns file into vector<vector<double>>
